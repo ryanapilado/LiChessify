142a991446d767f0952ccc940b36135c0319b0b8
Ryan Apilado <ryan.apilado@gmail.com>
2019-09-12 23:42:51 -0400

    Implement writing image from HTTP body to blob

--- lichessify-func/__init__.py
+++ lichessify-func/__init__.py
@@ -1,38 +1,56 @@
 import logging
 
 import azure.functions as func
+import azure.storage.blob as blob
 import paramiko
 import re
 import os
 import json
-
+import io
+import uuid
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+
     logging.info('Python HTTP trigger function processed a request.')
 
-    # temporary test input
-    board_image_url = "https://lichessifyfuncbba7.blob.core.windows.net/chessboards/2djjq4pmk4p31.jpg"
+    block_blob_service = blob.BlockBlobService(
+        account_name=os.environ["STORAGE_ACCOUNT_NAME"],
+        account_key=os.environ["STORAGE_ACCOUNT_KEY"]
+        )
 
+    # write image from HTTP body to blob
+    container_name = os.environ["STORAGE_CONTAINER_NAME"]
+    stream = io.BytesIO(req.get_body())
+    blob_name = "board_{0}.jpg".format(uuid.uuid4())
+    block_blob_service.create_blob_from_stream(container_name, blob_name, stream)
+
+    # get fen from blob using chessfenbot
     try:
-        fen, certainty = get_fen(board_image_url)
-        link = "https://lichess.org/analysis/{0}".format(fen)
-
+        blob_url = block_blob_service.make_blob_url(container_name, blob_name)
+        fen, certainty = get_fen(blob_url)
         body = {
-            "link":link,
+            "link":"https://lichess.org/analysis/{0}".format(fen),
             "certainty":certainty
         }
+
+        block_blob_service.delete_blob(container_name, blob_name)
         return func.HttpResponse(body=json.dumps(body), status_code=200)
+
     except:
+        block_blob_service.delete_blob(container_name, blob_name)
         return func.HttpResponse(status_code=400)
 
-
 def parse_fen_from_output(output: str) -> str:
+    ''' Retrieve the FEN and certainty from tensorflow_chessbot output.
+    '''
 
     match = re.search("Predicted FEN: (.*)\nFinal Certainty: (.*)%", output)
     assert(match)
     return match.group(1), float(match.group(2))
 
 def get_fen(board_image_url: str) -> str:
+    ''' SSH into the VM and run tensorflow_chessbot using board_image_url.
+    '''
 
     hostname = os.environ['CHESSBOT_IP']
     username = os.environ['CHESSBOT_USERNAME']
